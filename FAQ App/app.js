49e0505e434d474f397daa03a16b240c82aab783
marvelsam1 <142222829+marvelsam1@users.noreply.github.com>
2024-04-09 18:19:32 +0100

    Add files via upload

---
+++ FAQ App/app.js
@@ -0,0 +1,25 @@
+// const answer = document.querySelectorAll(".answer");
+const question = document.querySelectorAll(".article")
+const ans1 = document.getElementById("ans1");
+const ans2 = document.getElementById("ans2");
+const ans3 = document.getElementById("ans3");
+const ans4 = document.getElementById("ans4");
+const addBtn = document.querySelector(".add");
+const removeBtn = document.getElementById("remove");
+console.log(answer);
+console.log(addBtn);
+
+// addBtn[1].addEventListener("click", function () {
+//     answer[1].classList.add("showAnswer");
+// })
+// for(i = 0; i < addBtn.length; i++) {
+//     addBtn[i].addEventListener("click", function () {
+//        answer[i].classList.add("showAnswer")
+//     })
+// }
+
+const addAnswer = question.querySelector(".answer");
+addBtn.addEventListener("click", function () {
+    addAnswer.cla
+})
+ 
